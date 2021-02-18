58f0e4c61f91d0f4ad470f64743d49b3ec11e473
Frédéric Brière <fbriere@fbriere.net>
2021-02-17 22:47:39 -0500

    python3: Fix comparison between `None` and `int`
    `score_itsu()` used to return `None` instead of 0, which would cause a
    crash when compared.  This takes care to properly return 0, and removes
    the comparison just in case.  (There is no standard throughout the code
    on whether scores should be compared to zero or simply used as boolean.)

--- server/eval.py
+++ server/eval.py
@@ -277,7 +277,7 @@ def eval_sets(pair, sets, round_wind, player_wind, last_tile, wintype):
 	# Other hands
 	for name, fn in score_functions:
 		score = fn(pair, sets)
-		if score > 0:
+		if score:
 			result.append((name, score))
 
 	# Pinfu
@@ -385,6 +385,7 @@ def score_itsu(pair_tile, sets):
 			return 2
 		else:
 			return 1
+	return 0
 
 def score_chanta(pair_tile, sets):
 	if pair_tile.is_nonterminal() or not for_any_sets(sets, lambda s: s.is_chi()):
