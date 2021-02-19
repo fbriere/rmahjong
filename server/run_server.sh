b834c7f7ff59443226b40ddf0509e21037c05895
Frédéric Brière <fbriere@fbriere.net>
2021-02-19 16:56:35 -0500

    scripts: Automatically determine which python version to run

--- server/run_server.sh
+++ server/run_server.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
+
+find_python() {
+	# If no suitable candidate is found in the first pass, we look for any
+	# working interpreter in the second pass and let it display its own
+	# error message about missing modules.
+	for PYTHON_CMD in "$1" "pass"; do
+		for PYTHON_EXEC in python3 python2 python; do
+			if "$PYTHON_EXEC" -c "$PYTHON_CMD" >/dev/null 2>&1; then
+				echo "$PYTHON_EXEC"
+				return
+			fi
+		done
+	done
+}
+
+PYTHON="$(find_python "import six")"
+if [ ! "$PYTHON" ]; then
+	echo "Error: Could not find any working Python interpreter." >&2
+	exit 1
+fi
+
 cd "$(dirname "$0")"
-exec python server.py "$@"
+exec "$PYTHON" server.py "$@"
--- start.sh
+++ start.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
+
+find_python() {
+	# If no suitable candidate is found in the first pass, we look for any
+	# working interpreter in the second pass and let it display its own
+	# error message about missing modules.
+	for PYTHON_CMD in "$1" "pass"; do
+		for PYTHON_EXEC in python3 python2 python; do
+			if "$PYTHON_EXEC" -c "$PYTHON_CMD" >/dev/null 2>&1; then
+				echo "$PYTHON_EXEC"
+				return
+			fi
+		done
+	done
+}
+
+PYTHON="$(find_python "import six; import pygame; import OpenGL")"
+if [ ! "$PYTHON" ]; then
+	echo "Error: Could not find any working Python interpreter." >&2
+	exit 1
+fi
+
 cd "$(dirname "$0")/client"
-exec python client.py "$@"
+exec "$PYTHON" client.py "$@"
